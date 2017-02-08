1d4cd9a5717bcc493f1f04e5fe500d68b0c69ad2
Hugo Grochau <hugo@grochau.com>
2017-02-08 00:00:48 -0200

    Allow internal errors to go through

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,13 +1,20 @@
-const GlobalErrorHandler = () => ({
+export default () => ({
   response(next) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       next()
         .then((response) => resolve(response))
         .catch((response) => {
-          resolve(response);
+          try {
+            const body = JSON.parse(response.responseData);
+            if (body.status === 'error' && body.message) {
+              resolve(response);
+            } else {
+              reject(response);
+            }
+          } catch (e) {
+            reject(response);
+          }
         });
     });
   },
 });
-
-export default GlobalErrorHandler;
